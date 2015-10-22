6dc46e0eaf5ec2942051704af6a6ec08daf55ea5
Jb <jean-baptiste.dalle@laposte.net>
2015-10-22 17:45:09 +0200

    Correction merge

--- src/com/univ/angers/GeneralVariables.java
+++ src/com/univ/angers/GeneralVariables.java
@@ -17,12 +17,14 @@ public class GeneralVariables {
 	public final static String ROBOCODE_ENV = "ROBOCODE_HOME";
 	public final static String ROBOT_PACKAGE = "generation";
 	public final static String ROBOT_TEST_PACKAGE = "test";
+	public final static String BEST_ROBOT_PACKAGE = "best";
 	public final static String NEW_LINE = "\n";
+	public final static String DOUBLE_LINE = "\n\n";
 	public final static int BATTLE_HEIGHT = 600;
 	public final static int BATTLE_WIDTH = 800;
 	public final static int NUMBER_OF_ROUND = 5;
-	public final static int POPULATION_SIZE = 30;
-	public final static int NUMBER_OF_EVOLUTION = 100;
+	public final static int POPULATION_SIZE = 50;
+	public final static int NUMBER_OF_EVOLUTION = 10000;
 	public final static int NUMBER_OF_CHROMOSOME = 4;
 	public final static String BLOC_GENE_FILE_PATH = "";
 }
